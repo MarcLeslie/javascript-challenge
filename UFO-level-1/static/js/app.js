f9c52d2286d79a85bd10fc7e8f7c4f2f77215a78
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-06-23 17:11:32 -0400

    Basic code in - need a filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
+
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+//NEED A FILTER BUTTON
+
+data.forEach((UFO) => {
+    var row = tbody.append("tr");
+    Object.entries(UFO).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
