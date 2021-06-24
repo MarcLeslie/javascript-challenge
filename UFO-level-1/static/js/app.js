9b51db27a994010438c5802bb5d19cb597aa4872
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-06-24 11:07:04 -0400

    Pretty sure it is done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,36 +1,5 @@
-// from data.js
-
 var tableData = data;
-var tbody = d3.select("tbody");
-
-//NEED A FILTER BUTTON
-
-
-
-var button = d3.select("#filter-btn"); 
-var form = d3.select("#datetime"); 
-
-button.on("click" , runEnter); 
-form.on("submit" , runEnter); 
-
-// var tbody = d3.select("tbody"); //DOES THIS GO HERE?????
-
-function runEnter () {    //WHEN YOU CLICK SUBMIT, DO THE FOLLOWING...
-  d3.event.preventDefault();    //PREVENT PAGE FROM REFRESHING
-  var inputElement = d3.select("#datetime"); 
-  var inputValue = inputElement.property("value"); 
-  
-  console.log(`The input value is: ${inputValue}`); 
-  
-  console.log("The next thing should be tableData unfiltered"); 
-  console.log({tableData}); 
-
-  var filteredData = tableData.filter(x => x.datetime === inputValue);
-
-  console.log("The next thing should be tableData filtered"); 
-  console.log(filteredData); 
-}; 
-
+var tbody = d3.select("tbody");  //THIS BRINGS IN ALL THE DATA
 
 data.forEach((UFO) => {
   var row = tbody.append("tr");
@@ -39,3 +8,45 @@ data.forEach((UFO) => {
     cell.text(value);
   });
 });
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {    //WHEN YOU CLICK SUBMIT, DO THE FOLLOWING...
+  d3.event.preventDefault();    //PREVENT PAGE FROM REFRESHING
+  var inputElement = d3.select("#datetime"); // 99% sure that datetime is correct here
+  var inputValue = inputElement.property("value");
+
+  console.log(`The input value is: ${inputValue}`); // THAT THIS SHOWS THE DATE YOU ARE ENTERING MEANS IT IS WORKING 
+
+  // console.log("The next thing should be tableData unfiltered"); 
+  console.log({ tableData });
+
+  var filteredData = tableData.filter(x => x.datetime === inputValue);
+
+  // console.log("The next thing should be tableData filtered"); 
+  console.log(filteredData); //THIS DOES SHOW THE FILTERED DATA
+
+  tbody.html(""); //CLEAR 
+
+  filteredData.forEach((UFO) => {
+    var row = tbody.append("tr");
+    Object.entries(UFO).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
+
+var refreshButton = d3.select("#refresh-btn");
+refreshButton.on("click", runClearRefresh);
+function runClearRefresh() {
+  window.location.reload();
+  return false();
+};
+
+
+
