3c1263862fab8a560536308f58dc615606ce6a53
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-06-24 09:46:35 -0400

    I think i have the right pieces, just in the wrong places

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,17 +1,41 @@
 // from data.js
 
 var tableData = data;
-
 var tbody = d3.select("tbody");
 
 //NEED A FILTER BUTTON
 
+
+
+var button = d3.select("#filter-btn"); 
+var form = d3.select("#datetime"); 
+
+button.on("click" , runEnter); 
+form.on("submit" , runEnter); 
+
+// var tbody = d3.select("tbody"); //DOES THIS GO HERE?????
+
+function runEnter () {    //WHEN YOU CLICK SUBMIT, DO THE FOLLOWING...
+  d3.event.preventDefault();    //PREVENT PAGE FROM REFRESHING
+  var inputElement = d3.select("#datetime"); 
+  var inputValue = inputElement.property("value"); 
+  
+  console.log(`The input value is: ${inputValue}`); 
+  
+  console.log("The next thing should be tableData unfiltered"); 
+  console.log({tableData}); 
+
+  var filteredData = tableData.filter(x => x.datetime === inputValue);
+
+  console.log("The next thing should be tableData filtered"); 
+  console.log(filteredData); 
+}; 
+
+
 data.forEach((UFO) => {
-    var row = tbody.append("tr");
-    Object.entries(UFO).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
+  var row = tbody.append("tr");
+  Object.entries(UFO).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
   });
-
-
+});
